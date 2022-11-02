58fdefe37d2386262df441dafa19a0de8ab3317d
miltonpaull72 <miltonpaull72@gmail.com>
2022-11-02 11:23:29 -0500

    Libreria common-microservicios commit 1

---
+++ commons-microservicios/src/main/java/com/formaciobdi/microservicios/commons/services/CommonService.java
@@ -0,0 +1,21 @@
+package com.formaciobdi.microservicios.commons.services;
+
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+
+
+public interface CommonService<E> {
+	public Iterable<E> findAll();
+	
+	public Page<E> findAll(Pageable pageable);
+	
+	public Optional<E> findById(Long Id);
+	
+	public E save(E entity);
+	
+	public void deleteById(Long id);
+	
+}
